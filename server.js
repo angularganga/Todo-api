4b81d57e898c9e0de0e3477be24a980053d32307
unknown <ganganallur@gmail.com>
2018-04-09 21:31:04 -0400

    added allow origin

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ var todoNextId = 2;
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
